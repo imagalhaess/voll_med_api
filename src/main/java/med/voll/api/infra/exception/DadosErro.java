8990e98ff65a29e4217b61f04a0b64f3f2d2ba5a
imagalhaess <magalhaes569@gmail.com>
2025-12-30 13:44:21 -0300

    feat/[WIP]-authentication: implementa métodos de autenticação utilizando Spring Security e inicia introdução de BCrypt.

--- src/main/java/med/voll/api/infra/exception/DadosErro.java
+++
@@ -1,9 +0,0 @@
-package med.voll.api.infra.exception;
-
-import java.time.LocalDateTime;
-
-public record DadosErro(String mensagem, LocalDateTime timestamp) {
-    public DadosErro (String mensagem){
-        this(mensagem, LocalDateTime.now());
-    }
-}
