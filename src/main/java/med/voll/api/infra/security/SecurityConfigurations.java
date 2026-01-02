a13a1f2d44d242c73d29b1a79b5d2284c9677e62
imagalhaess <magalhaes569@gmail.com>
2026-01-02 18:09:18 -0300

    feat/documentation: implementação de biblioteca spring-docs e permissão das URLs fornecidas.

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -31,6 +31,7 @@ public class SecurityConfigurations {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers(HttpMethod.POST,"/login").permitAll();
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
 //                    req.requestMatchers(HttpMethod.DELETE, "/medicos/id").hasRole("ADMIN");
 //                    req.requestMatchers(HttpMethod.DELETE, "/pacientes/id").hasRole("ADMIN");
                     req.anyRequest().authenticated();
